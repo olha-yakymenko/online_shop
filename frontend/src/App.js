ca6259757636f422f35e41a1b83e9c756ad19214
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-12-25 13:23:38 +0100

    product komponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
+import Product from './Pages/Product';
 
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
         <BrowserRouter>
             <Routes>
               <Route path="/" element={<Shop />} />
+              <Route path="/product" element={<Product />}>
+                <Route path=":productId" element={<Product />} />
+              </Route>
             </Routes>
         </BrowserRouter>
       </div>
