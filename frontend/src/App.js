45a839a3208988e558952adf12a56b669f8b99c5
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2025-01-03 17:01:51 +0100

    context dla wyszukiwania

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,27 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import Product from './Pages/Product';
-
+import LoginSignup from './Pages/LoginSignup';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
-    return (
-      <div>
-        <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Shop />} />
-              <Route path="/product" element={<Product />}>
-                <Route path=":productId" element={<Product />} />
-              </Route>
-            </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <div>
+      <BrowserRouter>
+        <SearchContextProvider> 
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/product" element={<Product />}>
+              <Route path=":productId" element={<Product />} />
+            </Route>
+            <Route path="/login" element={<LoginSignup />} />
+          </Routes>
+        </SearchContextProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
   
