32c587eac22d99246811b89c080c3b7440a655c7
olha-yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2025-01-05 19:04:58 +0100

    styl dla shopCategory

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,51 +1,114 @@
+// import React, { useState, useContext } from 'react';
+// import './Navbar.css';
+// import logo from '../Assets/logo.png';
+// import cart_icon from '../Assets/cart_icon.png';
+// import { Link } from 'react-router-dom';
+// import { UserContext } from '../../Context/UserContext';
+// import Search from '../Search/Search';
+
+// const Navbar = () => {
+//     const [menu, setMenu] = useState("shop");
+//     const { user } = useContext(UserContext); 
+
+//     return (
+//         <div className="navbar">
+//             <div className="nav-logo">
+//                 <img src={logo} alt="logo" />
+//                 <p>YAK</p>
+//             </div>
+//             <ul className="nav-menu">
+//                 <li onClick={() => { setMenu("shop") }}>
+//                     <Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>
+//                     {menu === 'shop' ? <hr /> : null}
+//                 </li>
+//                 <li onClick={() => { setMenu("mens") }}>
+//                     <Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>
+//                     {menu === 'mens' ? <hr /> : null}
+//                 </li>
+//                 <li onClick={() => { setMenu("womens") }}>
+//                     <Link style={{ textDecoration: 'none' }} to='womens'>Women</Link>
+//                     {menu === 'womens' ? <hr /> : null}
+//                 </li>
+//                 <li onClick={() => { setMenu("kids") }}>
+//                     <Link style={{ textDecoration: 'none' }} to='kids'>Kids</Link>
+//                     {menu === 'kids' ? <hr /> : null}
+//                 </li>
+//             </ul>
+
+//             <Search />
+
+//             <div className="nav-login-cart">
+//                 {!user ? (
+//                     <Link to='/login'><button>Login</button></Link>
+//                 ) : (
+//                     <Link to='/cart'><img src={cart_icon} alt="Cart" /></Link>
+//                 )}
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Navbar;
+
+
+
+
 import React, { useState, useContext } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../Context/UserContext';
 import Search from '../Search/Search';
 
 const Navbar = () => {
-    const [menu, setMenu] = useState("shop");
-    const { user } = useContext(UserContext); 
+  const [menu, setMenu] = useState("shop");
+  const { user, logoutUser } = useContext(UserContext); 
+  const navigate = useNavigate()
 
-    return (
-        <div className="navbar">
-            <div className="nav-logo">
-                <img src={logo} alt="logo" />
-                <p>YAK</p>
-            </div>
-            <ul className="nav-menu">
-                <li onClick={() => { setMenu("shop") }}>
-                    <Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>
-                    {menu === 'shop' ? <hr /> : null}
-                </li>
-                <li onClick={() => { setMenu("mens") }}>
-                    <Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>
-                    {menu === 'mens' ? <hr /> : null}
-                </li>
-                <li onClick={() => { setMenu("womens") }}>
-                    <Link style={{ textDecoration: 'none' }} to='womens'>Women</Link>
-                    {menu === 'womens' ? <hr /> : null}
-                </li>
-                <li onClick={() => { setMenu("kids") }}>
-                    <Link style={{ textDecoration: 'none' }} to='kids'>Kids</Link>
-                    {menu === 'kids' ? <hr /> : null}
-                </li>
-            </ul>
+  const handleLogout = () => {
+    logoutUser(); 
+    navigate('/'); 
+  };
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p>YAK</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>
+          <Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>
+          {menu === 'shop' ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          <Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>
+          {menu === 'mens' ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("womens")}>
+          <Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link>
+          {menu === 'womens' ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          <Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>
+          {menu === 'kids' ? <hr /> : null}
+        </li>
+      </ul>
 
-            <Search />
+      <Search />
 
-            <div className="nav-login-cart">
-                {!user ? (
-                    <Link to='/login'><button>Login</button></Link>
-                ) : (
-                    <Link to='/cart'><img src={cart_icon} alt="Cart" /></Link>
-                )}
-            </div>
-        </div>
-    );
+      <div className="nav-login-cart">
+        {!user ? (
+          <Link to='/login'><button>Login</button></Link>
+        ) : (
+          <>
+            <Link to='/cart'><img src={cart_icon} alt="Cart" /></Link>
+            <button onClick={handleLogout} className="logout-button">Logout</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default Navbar; 
+export default Navbar;
